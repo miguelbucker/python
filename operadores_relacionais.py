e716028708e73fa0f4d2dec8e042d536bb0351e3
miguelbucker <miguel.silvabk@gmail.com>
2024-08-28 21:58:07 -0300

    Subindo Operadores Relacionais

---
+++ operadores_relacionais.py
@@ -0,0 +1,31 @@
+a = 5 
+b = 3
+
+## igualdade
+resultado = a==b
+print(resultado)
+
+## diferença
+resultado = a != b
+print(resultado)
+
+## Maior que (>)
+resultado = a > b
+print(resultado)
+
+## Menor que (<)
+resultado = a < b
+print(resultado)
+
+## Maior ou igual (>=)
+resultado = a >= b
+print(resultado)
+
+## Menor que (<=)
+resultado = a <= b
+print(resultado)
+
+
+
+
+
