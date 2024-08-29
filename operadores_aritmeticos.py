0b006ac9aa169fb6b32b300ef5ff1b3c215b0521
miguelbucker <miguel.silvabk@gmail.com>
2024-08-28 21:42:23 -0300

    Incluindo os arquivos operadores aritmeticos e jose mercado

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+a = 5
+b = 3
+c = 4
+
+# Adição (+)
+resultado = a+b+c
+print(f"Soma: {resultado}")
+
+# Subtração (-)
+resultado = c - b
+print(f"Subtração: {resultado}")
+
+# Multiplicação (*)
+resultado = b * a
+print(f"Multiplicação: {resultado}")
+
+# Divisão (/)
+resultado = a / b
+print(f"Divisão: {resultado}")
+
+# Divisão Inteira (//)
+resultado = a // b
+print(f"Divisão Inteira: {resultado}")
+
+
+# Resto da divisão (%)
+resultado = a % b
+print(f"Resto da divisão: {resultado}")
+
+# Exponencial (**)
+resultado = a ** b
+print(f"Exponencial: {resultado}")
