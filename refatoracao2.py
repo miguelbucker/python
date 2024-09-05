080454b430810d5d3786011b1e678aab9169abfa
miguelbucker <miguel.silvabk@gmail.com>
2024-09-04 21:08:29 -0300

    Primeira versão refatoração

---
+++ refatoracao2.py
@@ -0,0 +1,9 @@
+idade = 25
+
+categoria = "indefinida"
+if idade < 13:
+    categoria = "Criança"
+elif idade >= 13 and idade < 18:
+    categoria = "Adolescente"
+
+print ("A pessoa é classificada como", categoria)
