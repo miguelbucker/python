3a89df191ad282149658d723977cc8a8fbe8780f
miguelbucker <miguel.silvabk@gmail.com>
2024-09-04 21:43:56 -0300

    Aplicando em um código que pergunta se o palmeiras tem mundial

---
+++ palmeirasnaotemmundial.py
@@ -0,0 +1,13 @@
+# Fazendo pergunta
+pergunta = input("Palmeiras tem mundial?")
+
+# Definindo a resposta
+resposta = "indefinida"
+if pergunta == "Sim" :
+    resposta = "TÁ DE BRINKS ????"
+elif pergunta == "Não" :
+    resposta = "ACERTOOOU MIZERAVI"
+else :
+    resposta = "PALMEIRAS NÃO TEM MUNDIAL"
+
+print (f"{resposta}")
