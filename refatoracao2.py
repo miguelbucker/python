4593c64344ae978213a8c3d5b4e40134bd38317e
miguelbucker <miguel.silvabk@gmail.com>
2024-09-04 21:43:31 -0300

    Atualizando segunda versao do refatoracao

--- refatoracao2.py
+++ refatoracao2.py
@@ -1,9 +1,17 @@
-idade = 25
+# Solicitando entrada da variável idade
+idade = int(input("Qual sua idade?"))
 
-categoria = "indefinida"
-if idade < 13:
+# Definindo a categoria através da idade
+
+if idade < 0:
+    categoria = "TÁ DE BRINKS ????"
+elif idade < 13:
     categoria = "Criança"
 elif idade >= 13 and idade < 18:
-    categoria = "Adolescente"
+    categoria = "Adolescente"  
+elif idade >= 18 and idade < 60:
+    categoria = "Adulto"
+else :
+    categoria = "Idoso"
 
-print ("A pessoa é classificada como", categoria)
+print (f"A pessoa é classificada como: {categoria}")
