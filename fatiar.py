e3f8d31184148513130ab5301501376be9c69feb
miguelbucker <miguel.silvabk@gmail.com>
2024-09-05 21:41:56 -0300

    subindo arquivo fatiar

---
+++ fatiar.py
@@ -0,0 +1,17 @@
+# exemplo de string
+texto = "Ola,  Mundo!"
+
+# Fatiar do início até um indice específico
+print(texto[:5])
+
+# Fatiar a partir de um indice especifico até o final
+print(texto[6:])
+
+# Fatiar do inicio até o final, com um passo especifico
+print(texto[::2])
+
+# Fatiar com inicio e fim especificos
+print(texto[4:9])
+
+# Fatiar com inicio e fim especificos
+print(texto[::-1])
